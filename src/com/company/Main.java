65db003def8f6b411e108f59b02e355ffbddcc56
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-03-04 15:40:47 +0800

    Added operators, data types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        int value = 1;
+        int value2 = 3;
+        double value3 = 3142.14124;
+        int sum = value + value2;
+        int sum2 = (int) (sum + value3);
+
+        boolean isExisting = false;
+
+        if(isExisting){
+            System.out.println("print this");
+
+        }
+        System.out.println(sum2);
+
+        System.out.println(sum);
+
         System.out.println("Hello World");
     }
 }
